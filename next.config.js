27ef3905dd742c9e48b5a27a5ebe75b05e2d2244
giovanibee <giovani.barrios.arciga@outlook.com>
2024-01-03 22:44:48 -0800

    added workaround

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
+// eslint-disable-next-line
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	swcMinify: true,
 	typescript: {
 		ignoreBuildErrors: true,
 	},
+	webpack: (config, { isServer }) => {
+		if (isServer) {
+			config.plugins = [...config.plugins, new PrismaPlugin()]
+		}
+
+		return config
+	},
 }
 
+// eslint-disable-next-line
 module.exports = nextConfig
